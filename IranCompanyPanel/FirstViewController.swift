463398ae14b8d38f6882eab031afbb88a186ad36
Ryo <aryo@aryos-macbook-pro.local>
2017-02-17 10:59:11 +0330

    Initial Commit

---
+++ IranCompanyPanel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IranCompanyPanel
+//
+//  Created by Ryo on 11/29/1395 AP.
+//  Copyright © 1395 Ryo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
